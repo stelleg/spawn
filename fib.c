9d9a69ebbb1596b09bef604f496743da137be413
George Stelle <stelleg@gmail.com>
2015-07-10 09:04:01 -0600

    Working fib

--- fib.c
+++ fib.c
@@ -9,15 +9,18 @@ void fib(arg a){
     *ret = 1;
     return;
   }
-  long j, k;
-  spawn(fib, (arg){(void*)i-1, &j}, &q);
-  fib((arg){(void*)i-2, &k});
+  long j=0, k=0;
+  spawn(fib, (arg){(void*)(i-1), &j}, &q);
+  fib((arg){(void*)(i-2), &k});
+  while(!j) getwork(&q);
   *ret = j + k;
 }
 
 int main(){
   q = alloc_queue(1);
-  create_workers(1, &q);
-  spawn(fib, (arg){(void*)30}, &q);
-  printf("fib %d = %d \n", 30, 9);
+  create_workers(0, &q);
+  long j, i=35;
+  spawn(fib, (arg){(void*)i, &j}, &q);
+  while(!j) yield(&q);
+  printf("fib %d = %d \n", i, j);
 }
