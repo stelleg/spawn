1748997473487604a4ab2884b47c74c3d00b0ebe
George Stelle <stelleg@gmail.com>
2015-07-13 07:32:07 -0600

    Added hello finish statementg

--- fib.c
+++ fib.c
@@ -10,8 +10,8 @@ void fib(arg a){
     return;
   }
   volatile long j=0, k=0;
-  spawn(fib, (arg){(void*)(i-1), &j}, &q);
-  fib((arg){(void*)(i-2), &k});
+  spawn(fib, (arg){(void*)(i-1), (void*)&j}, &q);
+  fib((arg){(void*)(i-2), (void*)&k});
   while(!j || !k) getwork(&q);
   *ret = j + k;
 }
@@ -20,7 +20,7 @@ int main(){
   q = alloc_queue(1);
   create_workers(1, &q);
   volatile long j=0, i=35;
-  spawn(fib, (arg){(void*)i, &j}, &q);
+  spawn(fib, (arg){(void*)i, (void*)&j}, &q);
   while(!j) yield(&q);
   printf("fib %ld = %ld \n", i, j);
 }
--- hello.c
+++ hello.c
@@ -4,13 +4,18 @@
 queue q;
 
 void hello(arg a){
-  printf("Hello from %ld\n", (long ) a.r0);
+  printf("Hello from iteration %ld on thread %ld\n", (long ) a.r0, pthread_self());
+  *(long*)a.r1 += 1;
 }
 
 int main(){
   q = alloc_queue(8);
-  for(long i=0; i < 16; i++){
-    spawn(hello, (arg){(void*)i}, &q);
+  create_workers(3, &q);
+  long num_iters = 16;
+  volatile long counter = 0; 
+  for(long i=0; i < num_iters; i++){
+    spawn(hello, (arg){(void*)i, (void*)&counter}, &q);
   }
-  yield(&q);
+  while(counter < num_iters) getwork(&q);
+  printf("All hellos have been said\n");
 }
